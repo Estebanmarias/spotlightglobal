fd7d1098454b08242f148c9e1c03a4dad1371913
Estebanmarias <stephenbabatunde98@gmail.com>
2026-06-04 11:44:21 +0100

    F fix: update dependencies and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
-import type { Config } from 'tailwindcss'
+/** @type {import('tailwindcss').Config} */
 
-const config: Config = {
+
+const config = {
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
